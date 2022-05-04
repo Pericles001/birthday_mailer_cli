cd7bc073032a143de65d4d758da3786035cb7623
pericles001 <periclesadjovi@outlook.fr>
2022-05-04 14:16:20 +0100

    Starting with welcome method

--- mailer.js
+++ mailer.js
@@ -8,13 +8,16 @@
  *
  */
 
-(()=>{
-    console.log("USAGE : bd_mailer [--help] [--info] [--status] [--version] \n",
-                "<command> [<args>]\n",
-                "\n",
-                "A list of usual commands accepted by bd_mailer are exposed here:\n",
-                "\n",
-                "set personal informations up (email, firstname, lastname...)\n",
-                "\t config \t Configure personal mailer account"
-    )
-})()
+(() => {
+  console.log('USAGE : bd_mailer [--help] [--info] [--status] [--version] \n',
+    '<command> [<args>]\n',
+    '\n',
+    'A list of usual commands accepted by bd_mailer are exposed here:\n',
+    '\n',
+    'set your personal information up\n',
+    '\t config \t Configure personal mailer account\n',
+    '\n',
+    "set your receiver's information\n",
+    "\t to \t Configure receiver's personal details"
+  );
+})();
