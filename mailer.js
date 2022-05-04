0a821f263efbb5f4616b0b54f118f9d005ab079b
pericles001 <periclesadjovi@outlook.fr>
2022-05-04 04:55:55 +0100

    Added script file

---
+++ mailer.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+/**
+ * Script written to help automate birthday messages to friends and relatives
+ * via email
+ *
+ *
+ *
+ */
+
+(()=>{
+    console.log("USAGE : bd_mailer [--help] [--info] [--status] [--version] \n",
+                "<command> [<args>]\n",
+                "\n",
+                "A list of usual commands accepted by bd_mailer are exposed here:\n",
+                "\n",
+                "set personal informations up (email, firstname, lastname...)\n",
+                "\t config \t Configure personal mailer account"
+    )
+})()
