ca9bd658e9fb6ef6251fc1edddc33097c0febeb4
pericles001 <periclesadjovi@outlook.fr>
2022-05-07 04:58:52 +0100

    ried to test info const

--- src/mailer.js
+++ src/mailer.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+const help = require('./commands/defaults/help');
 
 /**
  * Script written to help automate birthday messages to friends and relatives
@@ -9,22 +10,5 @@
  */
 
 (() => {
-  console.log('USAGE : bd_mailer [--help] [--info] [--status] [--version] \n',
-    '<command> [<args>]\n',
-    '\n',
-    'A list of usual commands accepted by bd_mailer are exposed here:\n',
-    '\n',
-    'set your personal information up\n',
-    '\t config \t Configure personal mailer account\n',
-    '\n',
-    "set your receiver's information\n",
-    "\t to \t Configure receiver's personal details",
-    '\n',
-    "set your personal message\n",
-    "\t set \t Set your message text",
-    '\n',
-    "\t list \t List your friend's information\n",
-    "\t status  See status of your current transactions\n",
-    "\t abort \t Abort the message sending"
-  );
+    help();
 })();
