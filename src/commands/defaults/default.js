6dde6914a54167cb0b251060717b3b376a414d3a
pericles001 <periclesadjovi@outlook.fr>
2022-05-07 05:18:26 +0100

    Reviewed file presentation

--- src/commands/defaults/default.js
+++ src/commands/defaults/default.js
@@ -1,26 +1,27 @@
 const infos = require('./info');
 const helps = require('./help');
-const {argv} = require("process");
+const versions = require('./version');
 /**
  * Default  method of bd_mailer
  *
- * usage : display help informations
+ * usage : display native methods
  *          relative to bd_mailer
- * args : <h> <help> <-h> <-help>
- *         <--help>
+ * args : <h> <help> <i> <info>
+ *         <s> <status> <v> <version>
  *
  */
-
-
 
 const def = () => {
     const {argv} = require('process');
 
     if (argv[2] === undefined || argv[2] === 'help' || argv[2] === '--help') {
-       helps();
+        helps();
     } else if (argv[2] === 'i' || argv[2] === '-i' || argv[2] === '-info' || argv[2] === '--info' || argv[2] === 'info') {
         infos();
-    } else {
+    } else if (argv[2] === 'v' || argv[2] === '-v' || argv[2] === '-version'|| argv[2] === '--version'|| argv[2] === 'version'){
+        versions();
+    }
+    else {
         console.log(`Unknown option : ${argv[2]}`,
             '\nUSAGE : bd_mailer [--help] [--info] [--status] [--version] \n',
             '<command> [<args>]\n'
