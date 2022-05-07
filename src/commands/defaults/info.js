7e1a6364a7338494c3f7950aecccdb36ac7027a0
pericles001 <periclesadjovi@outlook.fr>
2022-05-07 05:18:01 +0100

    Reviewed file presentation

--- src/commands/defaults/info.js
+++ src/commands/defaults/info.js
@@ -1,4 +1,3 @@
-const {argv} = require("process");
 /**
  * Default information method of bd_mailer
  *
@@ -10,20 +9,17 @@ const {argv} = require("process");
  */
 
 const info = () => {
-    const {argv} = require('process');
-
-        console.log('\n',
-            'Birthday mailer made by Pericles001 is a simple messaging tool\n',
-            'for people who like terminal\n',
-            '\nPURPOSE: Automation of birthday messages\n',
-            '\t canals \t Gmail , telegram, whatsapp and simple sms\n',
-            '\n',
-            "This tool is expecting comments and advices from you to build\n",
-            "a strong community and develop much features\n",
-            '\n',
-            'Have a look at our features!\n',
-            );
-
+    console.log('\n',
+        'Birthday mailer made by Pericles001 is a simple messaging tool\n',
+        'for people who like terminal\n',
+        '\nPURPOSE: Automation of birthday messages\n',
+        '\t canals \t Gmail , telegram, whatsapp and simple sms\n',
+        '\n',
+        'This tool is expecting comments and advices from you to build\n',
+        'a strong community and develop much features\n',
+        '\n',
+        'Have a look at our features!\n'
+    );
 };
 
 module.exports = info;
