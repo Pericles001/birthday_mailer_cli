b68d8ab1b128b460c4cbe4a0690e557fc08e860d
pericles001 <periclesadjovi@outlook.fr>
2022-05-07 05:17:08 +0100

    Created version mentionning file

---
+++ src/commands/defaults/version.js
@@ -0,0 +1,17 @@
+/**
+ * Default version method of bd_mailer
+ *
+ * usage : display informations about version
+ *
+ * args : <v> <version> <-v> <-version>
+ *         <--version>
+ *
+ */
+
+const version = () => {
+    console.log('\n',
+        'bd_mailer version 1.0.0\n'
+    );
+};
+
+module.exports = version;
