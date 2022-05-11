028472f7d99f220ffcd42680a020fcc0a0f0aa33
pericles001 <periclesadjovi@outlook.fr>
2022-05-11 18:32:10 +0100

    Used readline to get user input

--- src/commands/config.js
+++ src/commands/config.js
@@ -1,20 +1,25 @@
-let user_infos = {};
-
+const userInfos = {};
+const readline = require('readline');
+const ask = readline.createInterface(
+  {
+    input: process.stdin,
+    output: process.stdout
+  });
 /**
  * Method to configure user information
  */
 
 const conf = () => {
-    console.log("This walktrough will guide you through creating your personal information's details",
-                '\n',
-                'See bd_mailer help init for more documented about this command fields',
-                '\n',
-                "Press ^C to exit at anytime"
-    );
-    user_infos.name = prompt("\t User name : (test)\t")
-    user_infos.gram = prompt("\t Telegram pseudo : (tester)\t")
-    user_infos.gram = prompt("\t Whatsapp pseudo : (tester)\t")
-    user_infos.email = prompt("\t Email address : (tester@gmail.com)\t")
-}
+  console.log("This walk-trough will guide you through creating your personal information's details",
+    '\n',
+    '\nSee bd_mailer help init for more documented about this command fields',
+    '\n',
+    '\nPress ^C to exit at anytime'
+  );
+  userInfos.name = ask('\t User name : (test)\t');
+  userInfos.gram = ask('\t Telegram pseudo : (tester)\t');
+  userInfos.what = ask('\t Whatsapp pseudo : (tester)\t');
+  userInfos.email = ask('\t Email address : (tester@gmail.com)\t');
+};
 
 module.exports = conf;
