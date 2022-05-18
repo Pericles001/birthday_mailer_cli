c715ba5735451a2f0f85ded922701846dbd6282a
pericles001 <periclesadjovi@outlook.fr>
2022-05-18 13:55:18 +0100

    Created const to store values locally

---
+++ src/stocks/store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+
+/**
+ * Script that store the data in the local stocks of the machine
+ */
+
+import {userInfos} from "../commands/configs/parameters.js";
+import {LocalStorage} from "node-localstorage";
+
+    const data = JSON.stringify(userInfos);
+    const storage = new LocalStorage('./storage', {
+        encoding: 'utf8',
+        error: function (err) {
+            console.log(err);
+        }
+    });
+export {storage, data};
