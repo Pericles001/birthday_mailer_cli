f12633959671f9279818a0f5f58b5717b8d0608f
pericles001 <periclesadjovi@outlook.fr>
2022-05-18 13:55:40 +0100

    Stored values into local storage via setter

--- src/commands/configs/email.js
+++ src/commands/configs/email.js
@@ -2,9 +2,16 @@
  * Script for email configs
  */
 
+import {ask, userInfos} from "./parameters.js";
+import {storage, data} from "../../stocks/store.js";
+
+
 const mail = () => ask.question('\t Email address : (tester@gmail.com)\t', (target) => {
     target ? console.log(`Pseudo is ${userInfos.mail = target}`) : console.log(`Pseudo is ${userInfos.mail = 'tester@gmail.com'}`);
+    storage.setItem('userInfos', JSON.stringify(Object.values(userInfos)));
     ask.close();
+    console.log(storage.getItem('userInfos'));
+
 });
 
 export {mail};
