30d110e4cb7718913a918b3c4b06f50a7608b8be
pericles001 <periclesadjovi@outlook.fr>
2022-05-26 16:53:47 +0100

    Imported day information and month information

---
+++ src/commands/configs/date/date.js
@@ -0,0 +1,16 @@
+/*
+* Script that is used to get the birthdate of the target
+*
+*/
+
+
+
+import {ask, targetDate} from "../parameters.js";
+import {getDay} from "./day.js";
+
+
+const getDate = () => {
+    getDay();
+};
+
+export {getDate};
