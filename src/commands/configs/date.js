683b7ca58795b7acdeec5cc518a79ec3bee40ca8
pericles001 <periclesadjovi@outlook.fr>
2022-05-26 16:30:06 +0100

    Started editing files to get target birthdate and birthday message

---
+++ src/commands/configs/date.js
@@ -0,0 +1,11 @@
+/*
+* Script that is used to get the birthdate of the target
+*
+*/
+
+
+const getDate = () => {
+
+}
+
+export {getDate};
---
+++ src/commands/configs/msg.js
@@ -0,0 +1,11 @@
+/*
+* Script that is used to get the birthdate message
+*
+*/
+
+
+const getMsg = () => {
+
+}
+
+export {getMsg};
