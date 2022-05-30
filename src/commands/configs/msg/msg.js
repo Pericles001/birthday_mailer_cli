afffdd82ee3c0f175eb08f6d9686a71fde10226b
pericles001 <periclesadjovi@outlook.fr>
2022-05-30 22:20:12 +0100

    Started creating msg file for edit

--- src/commands/configs/msg/msg.js
+++ src/commands/configs/msg/msg.js
@@ -4,9 +4,20 @@
 */
 
 import {targetMsg} from "../parameters.js";
+const {exec} = require('child_process');
 
 const getMsg = () => {
-    
+    exec('nano msg', (error, stdout, stderr)=> {
+        if (error) {
+            console.error(`error: ${error.message}`);
+            return;
+        }
+
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return;
+        }
+    })
 }
 
 export {getMsg};
