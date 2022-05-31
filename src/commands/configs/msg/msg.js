b4abbee8c6f0c44a4645574df09bd8b23daea3a2
pericles001 <periclesadjovi@outlook.fr>
2022-05-31 16:55:56 +0100

    Updated file with readfile, savemsg and get msg methods completion

--- src/commands/configs/msg/msg.js
+++ src/commands/configs/msg/msg.js
@@ -3,16 +3,48 @@
 *
 */
 
-import {editor, require} from "../parameters.js";
+import {editor, require, targetInfos, targetMsg} from "../parameters.js";
 import * as child_process from "child_process";
+import {storage} from "../../../storer/store.js";
+
+const fs = require('fs').promises;
+
+async function readFile(filePath) {
+    try {
+        const data = await fs.readFile(filePath);
+        console.log(data.toString());
+    } catch (error) {
+        console.error(`Got an error trying to read the file: ${error.message}`);
+    }
+}
+
+async function saveMsg(filePath) {
+    try {
+        const data = await fs.readFile(filePath);
+         targetMsg.content = data.toString();
+        storage.setItem('targetMsg', JSON.stringify(Object.values(targetMsg)));
+    } catch (error) {
+        console.error(`Got an error trying to save the file: ${error.message}`);
+    }
+}
+
 
 const getMsg = () => {
     const child = child_process.spawn(editor, ['../src/storage/message'], {
         stdio: 'inherit'
     });
     child.on('exit', function (e, code) {
-        console.log("finished");
+
+        saveMsg('../src/storage/message').then(r => console.log("Finished"));
     });
 
 }
-export {getMsg};
+
+
+
+
+
+
+
+
+export {getMsg, readFile};
