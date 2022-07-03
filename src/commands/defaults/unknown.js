19450606219ebcad0a485013c06d9e5194ddd836
pericles001 <periclesadjovi@outlook.fr>
2022-07-03 15:42:58 +0100

    Update unknown.js -Import constant ask -Add ask.close statement

--- src/commands/defaults/unknown.js
+++ src/commands/defaults/unknown.js
@@ -3,10 +3,13 @@
  * error
  */
 
+import {ask} from "../configs/parameters.js";
+
 const unknown = (args) => {
     console.log(`Unknown option : ${args}`,
         '\nUSAGE : bd_mailer [--help] [--info] [--status] [--version] \n',
         '<command> [<args>]\n')
+    ask.close();
 }
 
 export {unknown};
